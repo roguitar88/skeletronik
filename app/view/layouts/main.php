cda0a81046031fcb92ebc8660b52e666aa4107dc
unknown <rogeriobsoares5@gmail.com>
2022-06-28 13:25:38 -0300

    updating css and js assets in html]

--- app/view/layouts/main.php
+++ app/view/layouts/main.php
@@ -7,12 +7,13 @@
     <meta name="keywords" content="<?= $this->getMetaKeywords() ?>"/>
     <link rel="shortcut icon" href="<?= DIRPAGE ?>favicon.ico" type="image/x-icon">
     <link rel="icon" href="<?= DIRPAGE ?>favicon.ico" type="image/x-icon">
-    <script src="https://kit.fontawesome.com/dd21f273f4.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/e7ed460553.js" crossorigin="anonymous"></script>
     <script type="text/javascript" src="<?= DIRJS ?>jquery.js"></script>
 
     <!-- Bootstrap CSS -->
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
-    <link rel="stylesheet" type="text/css" href="<?= DIRBOOTSTRAP ?>dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="<?= DIRBOOTSTRAP ?>dist/css/bootstrap.min.css" crossorigin="anonymous" />
+    <!-- integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" -->
 
     <link rel="stylesheet" type="text/css" href="<?= DIRCSS ?>main.css"/>
 
@@ -47,7 +48,8 @@
 */
 ?>
 
-<script src="<?= DIRBOOTSTRAP ?>dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="<?= DIRBOOTSTRAP ?>dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<!-- integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" -->
 
 </body>
 </html>
